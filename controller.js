7dc13dbe694368c2ff98736e52599bce2daa42e9
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-04-07 23:24:32 +0700

    airell

---
+++ controller.js
@@ -0,0 +1,35 @@
+const Model = require('./model')
+const View = require('./view')
+
+class Controller {
+    static showHelp() {
+        View.message('Please add your Command:')
+        View.showHelp()
+    }
+    static showList(){
+        let tasks = Model.showList()
+        View.showList(tasks)
+    }
+    static add(input){
+        Model.add(input)
+        View.message(`Added "${input}" to your todo list...`)
+    }
+    static findById(input){
+        let found = Model.findById(input)
+        View.findById(found)
+    }
+    static delete(id){
+        Model.delete(id)
+        View.message(`Deleted id number : "${id}" from your todo list`)
+    }
+    static complete(id){
+        Model.complete(id)
+        View.message(`Task id number : ${id} has been changed`)
+    }
+    static uncomplete(id){
+        Model.uncomplete(id)
+        View.message(`Task id number : ${id} has been changed`)        
+    }
+}
+
+module.exports = Controller 
